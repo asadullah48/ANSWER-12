fa13ec93ea69888ac60b1fc1f1eabf573e1c489e
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2024-03-29 02:47:14 +0500

    Initial commit

---
+++ ans12.js
@@ -0,0 +1,5 @@
+var list = ["Usman", "Mansoor", "Mohsin", "Sultan", "Ifraheem", "Zakariyah", "Asadullah"];
+for (var _i = 0, list_1 = list; _i < list_1.length; _i++) {
+    var name_1 = list_1[_i];
+    console.log("Assalaam O Alikum ".concat(name_1, ", kindly attend iftar with me"));
+}
